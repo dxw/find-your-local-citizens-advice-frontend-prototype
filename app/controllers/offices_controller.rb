476d7f39f4f707cf2e441c240db53efe815c0c62
Tom Hipkin <tomhipkin@gmail.com>
2022-08-03 14:51:37 +0100

    Display the local authority name along with office results

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -1,15 +1,20 @@
 require 'net/http'
 class OfficesController < ApplicationController
   def index
-
     query = params["search"]
     query_params = params.permit("limit", "eligible_only").to_h.to_query
 
-    url = URI("http://localhost:3001/internal_postgres_search/#{query}?#{query_params}")
-    res = Net::HTTP.get(url)
+    if query.present?
+      url = URI("http://localhost:3001/internal_postgres_search/#{query}?#{query_params}")
+      res = Net::HTTP.get(url)
+      result_objects = JSON.parse(res, object_class: OpenStruct)
 
-    @offices = JSON.parse(res, object_class: OpenStruct) || []
-
+      @local_authority = result_objects.local_authority
+      @offices = result_objects.offices
+    else
+      @local_authority = nil
+      @offices = []
+    end
   end
 
   def show
