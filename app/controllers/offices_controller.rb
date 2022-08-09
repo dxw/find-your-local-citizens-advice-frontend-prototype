44f56f8b78ca5e4cbca09807774260b66ae020a0
Tom Hipkin <tomhipkin@gmail.com>
2022-08-09 14:56:35 +0100

    Remove unused local authority variable

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -9,8 +9,6 @@ class OfficesController < ApplicationController
       res = Net::HTTP.get(url)
       result_objects = JSON.parse(res, object_class: OpenStruct)
 
-      @local_authority = result_objects.results.local_authority
-
       offices_by_eligibility = result_objects.results.offices.group_by(&:eligible)
 
       @offices = offices_by_eligibility[nil]
